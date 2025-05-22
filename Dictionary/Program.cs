a700c1e9411bc56dbaf3636ce1cff8f174c46d80
shoaibalirts <shoaibali.rts@gmail.com>
2025-05-22 12:47:41 +0200

    Add project files.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<int, string> employees = new Dictionary<int, string>()
+            {
+                {101,"Shoaib Ali" },
+                {102, "Muhammad Ahmed" },
+                {103, "Ali Ahmed" }
+            };
+            Console.WriteLine("Key: Value");
+            Console.WriteLine("-----------");
+            foreach (KeyValuePair<int, string> employee in employees)
+            {
+                Console.WriteLine(employee.Key + ": " + employee.Value);
+            }
+            Console.WriteLine("-----------");
+
+        }
+    }
+}
